a3674bfd0f1e298c4c860dd8edfd103246ffb687
njmyers <17010801+njmyers@users.noreply.github.com>
2018-02-09 09:48:33 -0500

    added markdown to page content

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 
 Plugin Name: Raw Post Content
 Plugin URI: http://github.com:njmyers/raw-post
-Description: Add wordpress raw unrendere and markdown content to wp-json
+Description: Add wordpress raw post and page unrendered and markdown content to wp-json 
 Version: 0.1
 Author: Nick Myers
 
@@ -23,13 +23,13 @@ add_action( 'rest_api_init', 'add_raw_post_content');
 function add_raw_post_content() {
 
     register_rest_field( 'post', 'raw', array(
-        'get_callback'      => 'get_raw_content',
+        'get_callback'      => 'get_raw_post_content',
         'schema'            => null,
         )
     );
 };
 
-function get_raw_content( $object ) {
+function get_raw_post_content( $object ) {
     $post_id = $object['id'];
 
     $converter = new HtmlConverter(array('strip_tags' => true));
@@ -39,6 +39,32 @@ function get_raw_content( $object ) {
         'content'   => $post_data['post_content'],
         'title'     => $post_data['post_title'],
         'markdown'  => $converter->convert($post_data['post_content']),
+    );
+
+    return $raw_fields;
+};
+
+add_action( 'rest_api_init', 'add_raw_page_content');
+
+function add_raw_page_content() {
+
+    register_rest_field( 'page', 'raw', array(
+        'get_callback'      => 'get_raw_page_content',
+        'schema'            => null,
+        )
+    );
+};
+
+function get_raw_page_content( $object ) {
+    $page_id = $object['id'];
+
+    $converter = new HtmlConverter(array('strip_tags' => true));
+
+    $page_data = get_post( $page_id, 'ARRAY_A' );
+    $raw_fields = array(
+        'content'   => $page_data['post_content'],
+        'title'     => $page_data['post_title'],
+        'markdown'  => $converter->convert($page_data['post_content']),
     );
 
     return $raw_fields;
