c5ab674cd4fdd6fcc4ec8616fc5f8b0f766650ff
njmyers <17010801+njmyers@users.noreply.github.com>
2018-03-02 14:27:19 -0500

    rewrote plugin to add seperate fields for raw/markdown. converting markdown from rendered content fields

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 
 /*
 
-Plugin Name: Raw Post Content
+Plugin Name: Raw & Markdown Content for Posts & Pages
 Plugin URI: http://github.com:njmyers/raw-post
-Description: Add wordpress raw post and page unrendered and markdown content to wp-json 
-Version: 0.1.4
+Description: Add wordpress raw and markdown content for posts and pages to wp-json 
+Version: 0.1.5
 Author: Nick Myers
 
- */
+*/
 
 $autoload_path = __DIR__ . '/vendor/autoload.php';
 
@@ -18,56 +18,85 @@ if ( file_exists( $autoload_path ) ) {
 
 use League\HTMLToMarkdown\HtmlConverter;
 
-add_action( 'rest_api_init', 'add_raw_post_content');
+/*
+    These are the formatters
+ */
 
-function add_raw_post_content() {
-
-    register_rest_field( 'post', 'raw', array(
-        'get_callback'      => 'get_raw_post_content',
-        'schema'            => null,
-        )
-    );
-};
-
-function get_raw_post_content( $object ) {
+function get_markdown_content() {
     $post_id = $object['id'];
 
     $converter = new HtmlConverter(array('strip_tags' => true));
 
-    $post_data = get_post( $post_id, 'ARRAY_A' );
+    // get the raw data
+    $post_data = get_post( $post_id );
+
+    // apply wordpress renderers
+    $rendered_content = apply_filters('the_content', $post_data->post_content);
+    $rendered_title = apply_filters('the_title', $post_data->post_title);
+
+    // create array of converted HTML
+    $markdown_fields = array(
+        'content'   => $converter->convert($rendered_content),
+        'title'     => $converter->convert($rendered_title),
+    );
+
+    return $markdown_fields;
+}
+
+function get_raw_content( $object ) {
+    $post_id = $object['id'];
+
+    // get the raw data
+    $post_data = get_post( $post_id );
+
+    // create array of raw data
     $raw_fields = array(
-        'content'   => $post_data['post_content'],
-        'title'     => $post_data['post_title'],
-        'markdown'  => $converter->convert($post_data['post_content']),
+        'content'   => $post_data->post_content,
+        'title'     => $post_data->post_title,
     );
 
     return $raw_fields;
 };
 
-add_action( 'rest_api_init', 'add_raw_page_content');
+function add_raw_post_content() {
 
-function add_raw_page_content() {
-
-    register_rest_field( 'page', 'raw', array(
-        'get_callback'      => 'get_raw_page_content',
+    register_rest_field( 'post', 'raw', array(
+        'get_callback'      => 'get_raw_content',
         'schema'            => null,
         )
     );
 };
 
-function get_raw_page_content( $object ) {
-    $page_id = $object['id'];
+function add_markdown_post_content() {
 
-    $converter = new HtmlConverter(array('strip_tags' => true));
-
-    $page_data = get_post( $page_id, 'ARRAY_A' );
-    $raw_fields = array(
-        'content'   => $page_data['post_content'],
-        'title'     => $page_data['post_title'],
-        'markdown'  => $converter->convert($page_data['post_content']),
+    register_rest_field( 'post', 'markdown', array(
+        'get_callback'      => 'get_markdown_content',
+        'schema'            => null,
+        )
     );
-
-    return $raw_fields;
 };
+
+function add_raw_page_content() {
+
+    register_rest_field( 'page', 'raw', array(
+        'get_callback'      => 'get_raw_content',
+        'schema'            => null,
+        )
+    );
+}
+
+function add_markdown_page_content() {
+
+    register_rest_field( 'page', 'markdown', array(
+        'get_callback'      => 'get_markdown_content',
+        'schema'            => null,
+        )
+    );
+}
+
+add_action( 'rest_api_init', 'add_raw_post_content');
+add_action( 'rest_api_init', 'add_markdown_post_content');
+add_action( 'rest_api_init', 'add_raw_page_content');
+add_action( 'rest_api_init', 'add_markdown_page_content');
 
 ?>
